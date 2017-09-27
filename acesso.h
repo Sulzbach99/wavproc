ab2a350590d60e3f25b5975d8f120366b9860ae8
Sulzbach99 <lucas@sulzbach.org>
2017-09-26 21:03:38 -0300

    Submetendo projeto3

---
+++ acesso.h
@@ -0,0 +1,17 @@
+typedef struct audio
+    {
+        const char *ChunkID = "RIFF";
+        unsigned int ChunkSize;
+        const char *Format = "WAVE";
+        const char *SubChunk1ID = "fmt";
+        unsigned int SubChunk1Size;
+        unsigned short AudioFormat;
+        unsigned short ChannelNr;
+        unsigned int SampleRate;
+        unsigned int ByteRate;
+        unsigned short BlockAlign;
+        unsigned short BitsPerSample;
+        const char *SubChunk2ID = "data";
+        unsigned int SubChunk2Size;
+        signed long *Data;
+    } audio_t;
